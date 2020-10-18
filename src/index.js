b15b8a41872a55c95c2369a75ac6311eebd2f7a1
Arcuman <46977740+Arcuman@users.noreply.github.com>
2020-10-18 20:07:27 +0300

    fead: add solved task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let map = new Map();
+    let stack = [];
+    bracketsConfig.forEach(subArray => {
+        map.set(subArray[1], subArray[0])
+    });
+    str.split("").forEach(element => {
+        if (stack.length > 0 && stack[stack.length - 1] == map.get(element)) {
+            stack.pop();
+        } else {
+            stack.push(element);
+        }
+    });
+    if (stack.length > 0)
+        return false;
+    return true;
 }
